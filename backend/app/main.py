26c513013fdf7ee92cf4aa867d4c9ae8906afac8
Nizor <ogbezuodenizor@yahoo.com>
2026-06-14 11:06:24 +0100

    feat: improved Ui experience

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes.assessment import (
     router as assessment_router
 )
@@ -11,5 +13,18 @@ app = FastAPI(
     title="CarbonSense AI"
 )
 
-app.include_router(assessment_router)
-app.include_router(premium_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    assessment_router
+)
+
+app.include_router(
+    premium_router
+)
